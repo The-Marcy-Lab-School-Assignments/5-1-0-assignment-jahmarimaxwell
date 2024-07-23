2d0bebd60390d34606a080bc22d3d59014a98f16
Mari300 <jahfly9@gmail.com>
2024-07-22 22:14:54 -0400

    finished

--- vite-project/src/model/CartItem.js
+++ vite-project/src/model/CartItem.js
@@ -1,7 +1,18 @@
 import getId from "../utils/getId";
 
 class CartItem {
-
+    constructor(name, price) {
+        this.id = getId();
+        this.name = name;
+        this.price = price;
+    }
+    getId() {
+        id = 1;
+        id++;
+    }
+    createItem(itemName, itemPrice) {
+        return new CartItem(itemName, itemPrice)
+    }
 }
 
 export default CartItem;
--- vite-project/src/model/ShoppingCart.js
+++ vite-project/src/model/ShoppingCart.js
@@ -2,6 +2,37 @@ import getId from "../utils/getId";
 import CartItem from "./CartItem";
 
 class ShoppingCart {
+    #cartItems = [];
+
+
+    static #allCarts = [];
+    constructor() {
+        this.id = getId();
+        ShoppingCart.#allCarts.push(this);
+    }
+
+    createItem(name, price) {
+    const cart = new CartItem(name, price);
+    this.#cartItems.push(cart);
+    return cart;
+    }
+    getItems() {
+        return [...this.#cartItems];
+    }
+    removeItem(id) {
+        this.#cartItems.splice(this.#cartItems.findIndex((cart) => cart.id === id), 1);
+    } 
+    getTotal() {
+        return this.#cartItems.reduce((total, cartItem) => total + cartItem.price, 0);
+        /// looks through the cartItems aray and `reduces` / sums up the value of each cartItem's price
+        /// starting from 0
+    }
+    static listAll() {
+        return [...ShoppingCart.#allCarts];
+    }
+    static findBy(id) {
+        return ShoppingCart.#allCarts.find(cart => cart.id === id)
+    }
 
 }
 
